f513cdb229357b24430573598dcd3d93d9e1ffd5
suraj vankayala <suraj vankayala>
2016-08-03 22:18:17 -0400

    Added comments

--- js/disqus.js
+++ js/disqus.js
@@ -1,11 +1,25 @@
 
 
-var disqus_shortname = 'colah'; 
-    (function() {
-        var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-        dsq.src = 'http://disqus.com/forums/lambdaoinks/embed.js';
-        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-    })();
+var disqus_shortname = 'Vikash Kodati'; 
+    //(function() {
+      //  var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+    //    dsq.src = 'http://disqus.com/forums/lambdaoinks/embed.js';
+      //  (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+    //})();
+
+<div id="disqus_thread"></div>
+<script>
+    
+//(function() { // DON'T EDIT BELOW THIS LINE
+//    var d = document, s = d.createElement('script');
+//    s.src = '//vikash-kodati-blog.disqus.com/embed.js';
+//    s.setAttribute('data-timestamp', +new Date());
+//    (d.head || d.body).appendChild(s);
+//})();
+</script>
+<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+                                    
+
 
     (function () {
       var s = document.createElement('script'); s.async = true;
@@ -31,3 +45,6 @@ jQuery(document).ready(function() {
       maxWidth: 4000,
     });
 });
+
+
+
