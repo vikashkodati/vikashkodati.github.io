2ca14a34123faceb613b43bd3881521321b3e368
Vikash Kodati <vikash.kodati@gmail.com>
2024-08-20 21:55:32 +0000

    refactor: added common navbar to all pages.. this commit only has 1 page for testing

---
+++ loadNavbar.js
@@ -0,0 +1,9 @@
+// loadNavbar.js
+document.addEventListener("DOMContentLoaded", function() {
+    fetch('navbar.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('navbar-placeholder').innerHTML = data;
+        })
+        .catch(error => console.error('Error loading navbar:', error));
+});
